9b61e36bb745788c979a8ed83d1ca72caf6e85a8
Christopher Terrones <a20182048@pucp.edu.pe>
2022-06-06 20:43:43 -0500

    servlet terminado

--- src/main/java/Servlets/Servlet_ReporteFuturos.java
+++ src/main/java/Servlets/Servlet_ReporteFuturos.java
@@ -1,15 +1,19 @@
 package Servlets;
-
+import Bean.reporte;
+import Daos.reporte_futurosDao;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "Servlet_ReporteFuturos", value = "/Servlet_ReporteFuturos")
 public class Servlet_ReporteFuturos extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("webapp/reporte_futuros.jsp");
+        reporte_futurosDao reporteFuturosDao = new reporte_futurosDao();
+        request.setAttribute("listaToursporCiudad", reporteFuturosDao.obtenerReporte());
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("reporte_futuros.jsp");
         requestDispatcher.forward(request,response);
     }
 
